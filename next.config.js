739adf9944fad2303aae2b307745a01c0f86df36
Dang Dinh Nguyen <banhvannguyen45@gmail.com>
2026-05-20 15:44:56 +0700

    Config lai de deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Static site optimization
-  trailingSlash: true,
+  trailingSlash: true, // Tối ưu routing cho file tĩnh (Của bạn)
+  
+  // BẮT BUỘC: Phải có 2 dòng này thì web mới nhận được CSS/JS trên GitHub Pages
+  basePath: '/E-commerce', 
+  assetPrefix: '/E-commerce/',
 };
 
 module.exports = nextConfig;
