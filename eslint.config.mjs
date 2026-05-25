70eb6a6ba7f75e3085fa158c7ed9f6f7e98738ed
Dang Dinh Nguyen <banhvannguyen45@gmail.com>
2026-05-25 15:28:58 +0700

    finall

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,9 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 export default [
-  ...compat.extends("next/core-web-vitals"),
   {
     ignores: [".next/**", "out/**", "node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    rules: {},
   },
 ];
